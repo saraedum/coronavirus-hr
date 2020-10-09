ae05f679566863da21bff0e15ecaf9e51fdb6071
Julian Rüth <julian.rueth@fsfe.org>
2020-10-09 12:58:46 +0200

    add static rendering
    since nbviewer's caching seems to be stuck

--- scripts/rerender-notebook.sh
+++ scripts/rerender-notebook.sh
@@ -8,3 +8,6 @@ jupyter nbconvert Koronavirus.ipynb --to notebook --inplace --execute
 
 # Trust notebook so the plots show immediately.
 jupyter trust Koronavirus.ipynb
+
+# Rerender static HTML notebook
+jupyter nbconvert Koronavirus.ipynb --to html --stdout --no-input > index.html
