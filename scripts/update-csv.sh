f1020df935c1c2e6fb7cc600c851c230a4d6c664
Julian Rüth <julian.rueth@fsfe.org>
2021-02-28 18:52:07 +0100

    bypass cloudflare with scrapingant

--- scripts/update-csv.sh
+++ scripts/update-csv.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-./parse-koronavirus-hr.py <(curl -L -v --cookie "__cfduid=d2634b8dadda375deeea23d66af8433141612828153" https://www.koronavirus.hr) >> ../koronavirus.csv
+./parse-koronavirus-hr.py <(curl --request GET --url 'https://api.scrapingant.com/v1/general?url=https%3A%2F%2Fkoronavirus.hr' --header 'x-api-key: 6fe6249e2eb24c0fb61462cc4e290e47') >> ../koronavirus.csv
 
 # Eliminate duplicate lines in koronavirus.csv. Sometimes the reported data changes during the day so we only keep the last one we got on that day:
 tac ../koronavirus.csv | awk -F"," '!_[$1]++' | tac > ../koronavirus.csv.new
