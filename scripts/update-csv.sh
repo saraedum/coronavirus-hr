cbf603f645d5dfeff21a047bf233db8ed45f3cee
Julian Rüth <julian.rueth@fsfe.org>
2021-02-28 19:20:13 +0100

    use a private mirro since no cloudflare evasion seems to work

--- scripts/update-csv.sh
+++ scripts/update-csv.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-./parse-koronavirus-hr.py <(curl --request GET --url 'https://api.scrapingant.com/v1/general?url=https%3A%2F%2Fkoronavirus.hr' --header 'x-api-key: 6fe6249e2eb24c0fb61462cc4e290e47') >> ../koronavirus.csv
+./parse-koronavirus-hr.py <(curl https://julian-rueth.com/koronavirus-hr/index.html) >> ../koronavirus.csv
 
 # Eliminate duplicate lines in koronavirus.csv. Sometimes the reported data changes during the day so we only keep the last one we got on that day:
 tac ../koronavirus.csv | awk -F"," '!_[$1]++' | tac > ../koronavirus.csv.new
