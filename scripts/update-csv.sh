423e0013df2882512bf115caefdefd61b98b4263
Julian Rüth <julian.rueth@fsfe.org>
2021-02-28 12:31:49 +0100

    debug wget errors when pulling from koronavirus.hr

--- scripts/update-csv.sh
+++ scripts/update-csv.sh
@@ -3,7 +3,10 @@ set -e
 
 cd "$(dirname "$0")"
 
-./parse-koronavirus-hr.py <(wget koronavirus.hr --quiet -O -) >> ../koronavirus.csv
+tmp=`mktemp`
+wget koronavirus.hr -O $tmp
+
+./parse-koronavirus-hr.py $tmp >> ../koronavirus.csv
 
 # Eliminate duplicate lines in koronavirus.csv. Sometimes the reported data changes during the day so we only keep the last one we got on that day:
 tac ../koronavirus.csv | awk -F"," '!_[$1]++' | tac > ../koronavirus.csv.new
