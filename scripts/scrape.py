8ded46fba009ea913325cc108b6f6c24ae952baa
Julian Rüth <julian.rueth@fsfe.org>
2021-02-28 18:40:45 +0100

    use manually built cloudflare cookie to bypass protections for some time

--- scripts/scrape.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import cloudscraper
-
-scraper = cloudscraper.create_scraper()
-print(scraper.get("https://www.koronavirus.hr").text)
--- scripts/update-csv.sh
+++ scripts/update-csv.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-./parse-koronavirus-hr.py <(./scrape.py) >> ../koronavirus.csv
+./parse-koronavirus-hr.py <(curl -L -v --cookie "__cfduid=d2634b8dadda375deeea23d66af8433141612828153" https://www.koronavirus.hr) >> ../koronavirus.csv
 
 # Eliminate duplicate lines in koronavirus.csv. Sometimes the reported data changes during the day so we only keep the last one we got on that day:
 tac ../koronavirus.csv | awk -F"," '!_[$1]++' | tac > ../koronavirus.csv.new
