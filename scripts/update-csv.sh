3a63080233592513aeb305419229e0c535679704
Julian Rüth <julian.rueth@fsfe.org>
2021-02-28 17:32:53 +0100

    trying curl instead of wget to see if that helps with the 503 errors

--- scripts/update-csv.sh
+++ scripts/update-csv.sh
@@ -3,11 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-tmp=`mktemp`
-# The cloudflare setup seems to rate limit github so we get 503 errors. Use an open proxy instead.
-http_proxy=192.109.165.129 wget https://www.koronavirus.hr -O $tmp
-
-./parse-koronavirus-hr.py $tmp >> ../koronavirus.csv
+./parse-koronavirus-hr.py <(curl -L koronavirus.hr) >> ../koronavirus.csv
 
 # Eliminate duplicate lines in koronavirus.csv. Sometimes the reported data changes during the day so we only keep the last one we got on that day:
 tac ../koronavirus.csv | awk -F"," '!_[$1]++' | tac > ../koronavirus.csv.new
